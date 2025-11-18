8614d80d594a316b1e7f4dc8f65b39af0abedbad
braxterSSJ <gp922991@gmail.com>
2025-11-18 14:35:50 -0500

    Update Tabla.jsx

--- src/componentes/Tabla.jsx
+++ src/componentes/Tabla.jsx
@@ -1,1 +1,22 @@
+// src/componentes/Tabla.jsx (verificar importación y uso)
 
+import React from "react";
+import "../style.css";
+import Persona2 from "./Persona2"; // <--- ESTE DEBE APUNTAR A './Persona2.jsx'
+import { useGlobal } from "./ContextoGlobal";
+import { textos } from "../data/traducciones";
+
+export default function Tabla() {
+    const { idioma } = useGlobal();
+    // ...
+    
+    return (
+        <>
+            <Persona2 /> {/* <-- Aquí se renderizan Carrusel, Buscador y Destinos */}
+
+            <section className="seccion-tabla mostrar-tabla">
+            {/* ... (Tabla Comparativa) */}
+            </section>
+        </>
+    );
+}
