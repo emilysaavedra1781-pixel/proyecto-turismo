e9ca2b66a5404b2684ebab17643a277bca1c0797
braxterSSJ <gp922991@gmail.com>
2025-11-18 14:43:39 -0500

    Update destinocaCarrusel.js

--- src/data/destinocaCarrusel.js
+++ src/data/destinocaCarrusel.js
@@ -1,1 +1,28 @@
+// src/data/destinosCarrusel.js
 
+export const destinosCarrusel = [
+  {
+    id: 1,
+    url: "/Imagen/carrusel/machu-picchu.jpg", 
+    alt: "Machu Picchu, Cusco",
+    leyenda: "Machu Picchu 🏔️",
+  },
+  {
+    id: 2,
+    url: "/Imagen/carrusel/huacachina.jpg",
+    alt: "Laguna de Huacachina, Ica",
+    leyenda: "Laguna de Huacachina 🏜️",
+  },
+  {
+    id: 3,
+    url: "/Imagen/carrusel/amazonas.jpg",
+    alt: "Selva Amazónica, Loreto",
+    leyenda: "Selva Amazónica 🌳",
+  },
+  {
+    id: 4,
+    url: "/Imagen/carrusel/colca.jpg",
+    alt: "Cañón del Colca, Arequipa",
+    leyenda: "Cañón del Colca 🦅",
+  },
+];
