c0a15703a4458c3eb446ce924ab057cbd8576c38
emilysaavedra1781-pixel <emilysaavedra.17.8.1@gmail.com>
2025-12-02 09:05:55 -0500

    Mensaje

--- api/send.js
+++ api/send.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         "Authorization": `Bearer ${process.env.RESEND_API_KEY}`,
       },
       body: JSON.stringify({
-        from: "tu-correo-verificado@tudominio.com",
+        from: "onboarding@resend.dev",
         to: [correo],
         subject: "Bienvenido a Proyecto Turismo",
         html: `<p>Hola <b>${nombre}</b>, bienvenido a tu proyecto!</p>`,
